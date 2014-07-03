5ce00cc2d2eb796912f7b904b3d60418098781bf
isaac <ishivvers@berkeley.edu>
2014-07-03 16:47:03 -0700

    finalized pcygni line characterization

--- spectools.py
+++ spectools.py
@@ -127,24 +127,29 @@ def pEW(x, y, pc):
         greg.append( dlam*( (pc[i]-y[i])/pc[i] ) )
     return np.sum(greg)
 
-def FWHM(x, y, peak_val=None, plot=False):
+def FWHM(x, y, continuum_level=None, plot=False, emission=False):
     '''
     Calculates and returns the FWHM for the spectral line y on x.
-     Assumes line is a well-behaved absorption line, doesn't work for emission.
+     Assumes line is a well-behaved line.
      If spectrum is noisy, assumes minimum value is the depth.
-     If peak_val is given, will calculate relative to that (instead of y_max)
+     If continuum_level is not given, assumes it to 1.0 (i.e.
+        a continuum-normalized spectrum)
     '''
-    if peak_val == None:
-        peak_val = np.max(y)
-    depth = peak_val - np.min(y)
-    y_hm = peak_val - 0.5 * depth
+    if continuum_level == None:
+            continuum_level = 1.0
+    if not emission:
+        depth = continuum_level - np.min(y)
+        y_hm = continuum_level - 0.5 * depth
+    else:
+        height = np.max(y) - continuum_level
+        y_hm = continuum_level + 0.5*height
     # find the intersections with y_hm as you move left to right
     edges = []
     fl, fr = False, False
     for i in range(len(x)):
         # look for left side first
         if not fl:
-            if y[i] < y_hm:
+            if (not emission and (y[i] < y_hm)) or (emission and (y[i] > y_hm)):
                 # found the left edge
                 fl = True
                 # local linear approximation to find xintercept
@@ -155,7 +160,7 @@ def FWHM(x, y, peak_val=None, plot=False):
         else:
             # now start looking for the right side
             if not fr:
-                if y[i] > y_hm:
+                if (not emission and (y[i] > y_hm)) or (emission and (y[i] < y_hm)):
                     # found the right edge
                     fr = True
                     # local linear approximation to find xintercept
@@ -177,28 +182,24 @@ def FWHM(x, y, peak_val=None, plot=False):
         plt.show()
     return edges[1] - edges[0]
 
-def parameterize_line(x, y, err, xmid, emission=False, plot=False, width=100.0, spline_smooth=None,
+def parameterize_line(x, y, err, xmid, emission=False, plot=False, width=100.0, smoothing_order=3,
                       line_container=None, test_fit=False, xyrange=None):
     '''
     Fit a functional form to the line centered at xmid in 
      the spectrum y on x with errors err (all array-like).
     <width> is the edge-window width in x-coords
-    <spline_smooth> defines the smoothing parameter for the spline based on the length of the wl array
+    <smoothing_order> defines the local smoothing order; higher order gives more complexity
     <line_container> can be an object to append all plotted lines to, so they can be removed later
     If <test_fit>, this attempts a few sanity checks on the line to discard bad fits, and <xyrange> must
      be a tuple with the xrange and yrange of the full spectrum.
     If emission = True, looks for an emission line not an absorption line.
-    Returns the line, the pseudocontinuum, the fit form, and the function
-     (x, y, y_pc, y_fit, f(x))
+    Returns the line, the pseudocontinuum, and the fit form
+     (x, y, y_pc, y_fit)
     '''
     if line_container != None:
         lc = line_container
     else:
         lc = []
-    if spline_smooth == None:
-        s = spline_smooth
-    else:
-        s = spline_smooth * len(x)
     if test_fit and type(xyrange)!=tuple:
         raise Exception('If test_fit is true, must include measures of full spectral range.')
     
@@ -227,8 +228,9 @@ def parameterize_line(x, y, err, xmid, emission=False, plot=False, width=100.0, 
             width *= 1.5
             attempts += 1
             continue
-        spline = UnivariateSpline( xx, yy, k=5, s=s )
-        yy2 = spline(xx)
+        smoother = smooth.LocalPolynomialKernel1D(xx, yy, q=smoothing_order)
+        yy2 = smoother( xx )
+
         if test_fit:
             ss = ((max(pc)-min(pc))/xyrange[1]) / ((max(xx)-min(xx))/xyrange[0])
             ww = 3e5 * (max(xx)-min(xx))/xmid
@@ -285,6 +287,44 @@ def parameterize_line(x, y, err, xmid, emission=False, plot=False, width=100.0, 
         plt.show()
     return xx, yy, ee, pc, yy2
 
+def calc_everything(x, y, err, xmid, emission=False, plot=0, width=100.0, line_container=None, smoothing_order=3):
+    '''
+    Calculates properties of a the line centered at xmid in 
+     the spectrum y on x.  <plot> can be one of [0,1,2], to produce
+     different levels of plots. <width> is the width of the edge windows
+     used to fit for the continuum. <spline_smooth> is a factor that defines
+     the spline_smooth factor based on size of the wl array.
+    Returns:
+     pEW
+     wl_min
+     rel_depth
+     FWHM
+    '''
+    if plot > 0:
+        p = True
+    else:
+        p = False
+    # xyrange = (np.max(x)-np.min(x), np.max(y)-np.min(y))
+    xyrange = (6000, np.max(y)-np.min(y)) # use a fixed wl range, so that the sanity checks aren't dependant on wl range
+    xx, yy, ee, pc, yy2 = parameterize_line(x,y,err,xmid, emission=emission, plot=p, width=width, smoothing_order=smoothing_order,
+                                        line_container=line_container, test_fit=True, xyrange=xyrange)
+    # the pseudo equivalent width
+    pew = pEW(xx,yy,pc)
+    # the wl of the minimum, where the minimum is the largest distance between pseudocontinuum and fit
+    imin = np.argmax(pc-yy2)
+    wl_min = xx[imin]
+    # the relative depth of the feature
+    relative = yy2/pc
+    rel_depth = 1.0 - np.min(relative)
+    # the FWHM
+    if plot > 1:
+        p = True
+    else:
+        p = False
+    fwhm = FWHM(xx, relative, 1.0, plot=p)
+    return pew, wl_min, rel_depth, fwhm
+
+
 def find_pcygni_pcont(x, y, err, xmid, plot=False, width=300.0):
     '''
     Find and remove the pseudocontinuum for the  p-cygni line centered
@@ -315,7 +355,7 @@ def find_pcygni_pcont(x, y, err, xmid, plot=False, width=300.0):
         plt.show()
     return xx, yy, ee, pc
 
-def parameterize_pcygni(x, y, err, xmid, plot=False, width=300.0, 
+def parameterize_pcygni(x, y, err, xmid, plot=False, width=300.0, smoothing_order=3,
                       line_container=None, test_fit=False, xyrange=None):
     '''
     Fit a functional form to the line centered at xmid in 
@@ -338,6 +378,12 @@ def parameterize_pcygni(x, y, err, xmid, plot=False, width=300.0,
     
     attempts = 0
     max_attempts = 3     # max number of adjustments to make when trying to fit the line
+    # parameters used by keyword "test_fit"
+    max_slope = 3.5      # max slope of pseudocontinuum, relative to width of full spectrum
+    max_width = 60000.0  # max width of feature in km/s
+    min_width = 5000.0   # min width of feature in km/s
+    max_offset = 15000.0 # max velocity offset of absorption feature in km/s
+    min_depth = 1.0      # minimum depth of line in standard deviations
     while True:
         if attempts > max_attempts:
             raise AssertionError('Could not successfully fit line at %.2f.'%xmid)
@@ -348,9 +394,54 @@ def parameterize_pcygni(x, y, err, xmid, plot=False, width=300.0,
             width *= 1.5
             attempts += 1
             continue
-        smoother = smooth.LocalPolynomialKernel1D(xx, yy, q=3)
+        smoother = smooth.LocalPolynomialKernel1D(xx, yy, q=smoothing_order)
         yy2 = smoother( xx )
-        break
+        
+        if test_fit:
+            ss = ((max(pc)-min(pc))/xyrange[1]) / ((max(xx)-min(xx))/xyrange[0]) #slope of pc relative to the spectrum
+            ww = 3e5 * (max(xx)-min(xx))/xmid                                    #width of feature in km/s
+            os = 3e5 * np.abs( (xx[np.argmin(yy2)]) - xmid ) / xmid              #velocity offset of absorption
+            depth = np.max(yy2-pc) + np.max(pc-yy2)                              #total depth of feature from peak to trough
+            if ss > max_slope:
+                print 'slope too steep'
+                # if slope is too steep, try making the edge width bigger
+                width *= 1.5
+                attempts += 1
+                continue
+            elif ww > max_width:
+                print 'feature too wide'
+                # if feature is too wide, try making the edge width smaller
+                width *= (2./3)
+                attempts += 1
+                continue
+            elif ww < min_width:
+                print 'feature too narrow'
+                # if feature is too narrow, try making the edge width bigger
+                width *= 1.5
+                attempts += 1
+                continue
+            elif os > max_offset:
+                print 'feature too far offset from center'
+                # probably chose the wrong feature; try making the edge width smaller
+                width *= (2./3)
+                attempts += 1
+                continue
+            elif depth < min_depth * np.std(yy):
+                print 'feature too shallow'
+                # try widening the feature
+                width *= 1.5
+                attempts += 1
+                continue
+            elif (np.argmin(yy2) == 0) or (np.min(yy2) == len(yy2)-1):
+                print 'bad parameterization'
+                # this happens if one of the edges is the minimum
+                width *= 1.5
+                attempts += 1
+                continue
+            else:
+                break
+        else:
+            break
 
     if plot:
         ls = plt.plot(xx,yy,'b')
@@ -362,18 +453,23 @@ def parameterize_pcygni(x, y, err, xmid, plot=False, width=300.0,
         plt.show()
     return xx, yy, ee, pc, yy2
 
-def calc_everything(x, y, err, xmid, emission=False, plot=0, width=100.0, spline_smooth=None, line_container=None):
+
+def calc_everything_pcygni(x, y, err, xmid, plot=0, width=300.0, line_container=None, smoothing_order=3):
     '''
-    Calculates properties of a the line centered at xmid in 
+    Calculates properties of a P-Cygni line centered at xmid in 
      the spectrum y on x.  <plot> can be one of [0,1,2], to produce
      different levels of plots. <width> is the width of the edge windows
-     used to fit for the continuum. <spline_smooth> is a factor that defines
-     the spline_smooth factor based on size of the wl array.
+     used to fit for the continuum. <smoothing_order> is the order of the 
+     local polynomial kernel used to smooth.
     Returns:
-     pEW
+     pEW_absorption
+     pEW_emission
      wl_min
+     wl_max
      rel_depth
-     FWHM
+     rel_height
+     FWHM_absorption
+     FWHM_emission
     '''
     if plot > 0:
         p = True
@@ -381,21 +477,34 @@ def calc_everything(x, y, err, xmid, emission=False, plot=0, width=100.0, spline
         p = False
     # xyrange = (np.max(x)-np.min(x), np.max(y)-np.min(y))
     xyrange = (6000, np.max(y)-np.min(y)) # use a fixed wl range, so that the sanity checks aren't dependant on wl range
-    xx, yy, ee, pc, yy2 = parameterize_line(x,y,err,xmid, emission=emission, plot=p, width=width, spline_smooth=spline_smooth,
+    xx, yy, ee, pc, yy2 = parameterize_pcygni(x,y,err,xmid, plot=p, width=width, smoothing_order=smoothing_order,
                                         line_container=line_container, test_fit=True, xyrange=xyrange)
-    # the pseudo equivalent width
-    pew = pEW(xx,yy,pc)
-    # the wl of the minimum, where the minimum is the largest distance between pseudocontinuum and fit
+    # the wl of the min and max, relative to the pseudocontinuum
     imin = np.argmax(pc-yy2)
     wl_min = xx[imin]
-    # the relative depth of the feature
-    relative = yy2/pc
-    rel_depth = 1.0 - np.min(relative)
-    # the FWHM
+    imax = np.argmax(yy2-pc)
+    wl_max = xx[imax]
+    # find the crossing point
+    icross = np.argmin( np.abs(yy2[imin:imax]-pc[imin:imax]) )
+    xcross = xx[imin:imax][icross]
+    # the pseudo equivalent width of the absorption
+    mabs = (xx<xcross)
+    pew_abs = pEW(xx[mabs], yy[mabs], pc[mabs])
+    # the pseudo equivalent width of the (inverted) emission
+    mess = (xx>=xcross)
+    pew_ems = pEW(xx[mess], 2*pc[mess]-yy[mess], pc[mess]) #warning: nonsense if emission line is much higher than continuum!
     if plot > 1:
         p = True
     else:
         p = False
-    fwhm = FWHM(xx, relative, 1.0, plot=p)
-    return pew, wl_min, rel_depth, fwhm
-
+    # the relative depth of the absorption feature
+    relative = yy2[mabs]/pc[mabs]
+    rel_depth = 1.0 - np.min(relative)
+    # the FWHM of the absorption
+    fwhm_abs = FWHM(xx[mabs], relative, 1.0, plot=p)
+    # relative height of the emission feature
+    relative = yy2[mess]/pc[mess]
+    rel_height = np.max(relative) - 1.0
+    # the FWHM of the emission
+    fwhm_ems = FWHM(xx[mess], relative, 1.0, plot=p, emission=True)
+    return pew_abs, pew_ems, wl_min, wl_max, rel_depth, rel_height, fwhm_abs, fwhm_ems
