bbe6977db2912a763130d6afdb1487244739dcb4
isaac <ishivvers@berkeley.edu>
2015-01-23 11:29:42 -0800

    expanded so now works with kast intermediate reduction fits

--- fits2flm.py
+++ fits2flm.py
@@ -14,19 +14,41 @@ def fits2flm( fitsfile, outfile ):
     h = pf.open( fitsfile )
     assert( h[0].header['ctype1'] == 'LINEAR' )
     wl0 = h[0].header['CRVAL1']
-    dwl = h[0].header['CD1_1']
-    wl = wl0 + np.arange( len(h[0].data) )*dwl
-    fl = h[0].data
+    try:
+        dwl = h[0].header['CD1_1']
+    except:
+        # sometimes a different keyword is used here
+        dwl = h[0].header['CDELT1']
+    if len(h[0].data.shape) == 1:
+        wl = wl0 + np.arange( len(h[0].data) )*dwl
+    else:
+        wl = wl0 + np.arange( len(h[0].data[0]) )*dwl
+    if len(h[0].data.shape) == 1:
+        fl = h[0].data
+        er = None
+    else:
+        # assume first array is fl, and second is er
+        fl = h[0].data[0]
+        er = h[0].data[1]
 
     outf = open(outfile, 'w')
     header = '# Converted from %s\n' %fitsfile +\
-             '# wl (A)     fl (flam) \n'
+             '# wl (A)     fl (flam)   er (flam, opt)\n'
     outf.write( header )
     if np.max( fl ) < 0.1:
-        line = '%.6f    %.6e\n'
+        if er == None:
+            line = '%.6f    %.6e\n'
+        else:
+            line = '%.6f    %.6e    %.6e\n'
     else:
-        line = '%.6f    %.6f\n'
+        if er == None:
+            line = '%.6f    %.6f\n'
+        else:
+            line = '%.6f    %.6f    %.6f\n'
     for i in range(len(wl)):
-        outf.write( line%(wl[i],fl[i]) )
+        if er == None:
+            outf.write( line%(wl[i],fl[i]) )
+        else:
+            outf.write( line%(wl[i],fl[i],er[i]) )
     outf.close()
     print 'saved %s to ascii format in %s.' %(fitsfile,outfile)
